ddf521802900e86351f2faca8da862e270e812eb
Rahul Chowdhury <rahul@mitter.io>
2018-09-19 18:01:03 +0530

    Add user auth

--- app/src/main/java/io/mitter/starterapp/MainActivity.kt
+++ app/src/main/java/io/mitter/starterapp/MainActivity.kt
@@ -2,11 +2,35 @@ package io.mitter.starterapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import io.mitter.android.Mitter
+import io.mitter.android.error.model.base.ApiError
+import io.mitter.data.domain.user.User
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mitter: Mitter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mitter = (application as MyApp).mitter
+
+        val users = mitter.Users()
+        val channels = mitter.Channels()
+        val messaging = mitter.Messaging()
+
+        users.getCurrentUser(
+            onValueAvailableCallback = object : Mitter.OnValueAvailableCallback<User> {
+                override fun onError(apiError: ApiError) {
+                    Log.d("MSA", "Error while fetching user: $apiError")
+                }
+
+                override fun onValueAvailable(value: User) {
+                    Log.d("MSA", "User is: ${value.screenName.screenName}")
+                }
+            }
+        )
     }
 }
--- app/src/main/java/io/mitter/starterapp/MyApp.kt
+++ app/src/main/java/io/mitter/starterapp/MyApp.kt
@@ -3,6 +3,7 @@ package io.mitter.starterapp
 import android.app.Application
 import io.mitter.android.Mitter
 import io.mitter.android.domain.model.MitterConfig
+import io.mitter.android.domain.model.UserAuth
 import io.mitter.models.mardle.messaging.*
 
 class MyApp : Application() {
@@ -14,9 +15,15 @@ class MyApp : Application() {
             applicationId = "c5add92b-c42e-447b-b0e1-24c9e9044dce"
         )
 
+        val userAuth = UserAuth(
+            userId = "8ed92f3c-0696-4513-a842-085e3cee589e",
+            userAuthToken = "eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJtaXR0ZXItaW8iLCJ1c2VyVG9rZW5JZCI6ImJtRlI5bWNQaDhkQnJHaWIiLCJ1c2VydG9rZW4iOiJiM2dvY2ZhZ3ZyNWlrNHJkbXJlc29wNnNlcyJ9.dlE1QOYmUJpqoh1kORm3hEI3KbBM0v8kKZQnQQwXR6TZuFiCaDQrJMlp-2dgNP1CTYCPMFYoqGctRWQ5JyNiOQ"
+        )
+
         mitter = Mitter(
             context = this,
-            mitterConfig = mitterConfig
+            mitterConfig = mitterConfig,
+            userAuth = userAuth
         )
 
         mitter.registerOnPushMessageReceivedListener(object : Mitter.OnPushMessageReceivedCallback {
