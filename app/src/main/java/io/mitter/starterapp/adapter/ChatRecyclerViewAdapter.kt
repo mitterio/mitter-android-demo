6bbcad4dab5b15c310b68fb9c0f08a22c907457f
Rahul Chowdhury <rahul@mitter.io>
2018-09-19 19:22:11 +0530

    Add layouts

---
+++ app/src/main/java/io/mitter/starterapp/adapter/ChatRecyclerViewAdapter.kt
@@ -0,0 +1,23 @@
+package io.mitter.starterapp.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class ChatRecyclerViewAdapter: RecyclerView.Adapter<ChatRecyclerViewAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class ViewHolder(itemView: View?): RecyclerView.ViewHolder(itemView) {
+
+    }
+}
