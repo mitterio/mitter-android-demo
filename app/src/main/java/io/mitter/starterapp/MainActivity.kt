bff61a939c0781f2363aba3df42a2489ac5fd4ef
Rahul Chowdhury <rahul@mitter.io>
2018-09-19 19:36:50 +0530

    Show messages in channel

--- app/src/main/java/io/mitter/starterapp/MainActivity.kt
+++ app/src/main/java/io/mitter/starterapp/MainActivity.kt
@@ -2,10 +2,14 @@ package io.mitter.starterapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import io.mitter.android.Mitter
 import io.mitter.android.error.model.base.ApiError
 import io.mitter.data.domain.user.User
+import io.mitter.models.mardle.messaging.Message
+import io.mitter.starterapp.adapter.ChatRecyclerViewAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,14 +25,20 @@ class MainActivity : AppCompatActivity() {
         val channels = mitter.Channels()
         val messaging = mitter.Messaging()
 
-        users.getCurrentUser(
-            onValueAvailableCallback = object : Mitter.OnValueAvailableCallback<User> {
+        chatRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        messaging.getMessagesInChannel(
+            channelId = "6dedfac5-8060-4ad3-8d69-20a72fb86899",
+            onValueAvailableCallback = object : Mitter.OnValueAvailableCallback<List<Message>> {
                 override fun onError(apiError: ApiError) {
-                    Log.d("MSA", "Error while fetching user: $apiError")
+                    Log.d("MSA", "Error in getting messages")
                 }
 
-                override fun onValueAvailable(value: User) {
-                    Log.d("MSA", "User is: ${value.screenName.screenName}")
+                override fun onValueAvailable(value: List<Message>) {
+                    chatRecyclerView.adapter = ChatRecyclerViewAdapter(
+                        messageList = value,
+                        currentUserId = mitter.getUserId()
+                    )
                 }
             }
         )
